eed57a9e60070b28cb0e31d321da08f24b3349cb
Adrien <adritor@orange.fr>
2021-05-31 17:49:30 +0200

    Update portfolio.sql

--- portfolio.sql
+++ portfolio.sql
@@ -20,7 +20,9 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `portfolio`
 --
-
+DROP DATABASE IF EXISTS portfolio;
+ CREATE DATABASE portfolio;
+ USE portfolio;
 -- --------------------------------------------------------
 
 --
